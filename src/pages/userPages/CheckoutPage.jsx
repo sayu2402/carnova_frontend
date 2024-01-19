f703a2fcca92310c836fd0ca08d44ce4d51d03ad
sayooj <sayooj.o12@gmail.com>
2024-01-19 17:58:27 +0530

    .env added

--- src/pages/userPages/CheckoutPage.jsx
+++ src/pages/userPages/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "../../containers/common/NavBar";
 import Footer from "../../containers/common/Footer";
-import Checkout from "../../containers/user/checkout";
+import Checkout from "../../containers/user/checkout"; 
 
 function CheckoutPage() {
   return (
