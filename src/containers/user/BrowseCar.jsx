02ec21fae6eacbb6e42bac8f38e4826bebe1eff5
sayooj <sayooj.o12@gmail.com>
2024-01-24 12:19:40 +0530

    loading removed from browsecar

--- src/containers/user/BrowseCar.jsx
+++ src/containers/user/BrowseCar.jsx
@@ -16,7 +16,6 @@ const baseUrl = process.env.REACT_APP_BASE_URL;
 const BrowseCar = () => {
   const { isLoaded } = useGoogleMapApi();
   const [location, setLocation] = useState("");
-  const [loading, setLoading] = useState(true);
   const [errorLocation, setErrorLocation] = useState("");
   const {
     values,
@@ -57,8 +56,6 @@ const BrowseCar = () => {
       });
     } catch (error) {
       console.error("Error fetching car data:", error);
-    }finally {
-      setLoading(false)
     }
   };
 
@@ -89,7 +86,6 @@ const BrowseCar = () => {
 
   return (
     <>
-    {loading && <Loading />}
     <div>
       <div className="bg-slate-200 py-20 pl-4">
         <h2 className="text-4xl font-bold text-black mb-4">Available Cars</h2>
